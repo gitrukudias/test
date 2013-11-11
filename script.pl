03b2a1f97c9334fdb8f2bfa1c3c7c99d18200138
dmas <dmas@dmas.(none)>
2013-11-11 02:08:23 +0200

    add new file pl

---
+++ script.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+if ( $hour < 10 ) { print "Good morning!\n"; }
+elsif ( $hour == 12 ) { print "Out for lunch...\n"; }
+elsif ( $hour <= 18 ) { print "Hello world!\n"; }
+elsif ( $hour < 23 )  { print "Good night.\n"; }
+else { print "sleep well!\n"; }
