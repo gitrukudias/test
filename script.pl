82c58b091d20b37f08aa63cf1e0c664f180ec086
dmas <dmas@dmas.(none)>
2013-11-11 02:14:53 +0200

    add some changes

--- script.pl
+++ script.pl
@@ -5,3 +5,4 @@ elsif ( $hour == 12 ) { print "Out for lunch...\n"; }
 elsif ( $hour <= 18 ) { print "Hello world!\n"; }
 elsif ( $hour < 23 )  { print "Good night.\n"; }
 else { print "sleep well!\n"; }
+else { print "sleep well!\n"; }
